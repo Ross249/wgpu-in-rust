daea6080f694a9cab181cd30760ad14811ec4ea7
Ross <528266131@qq.com>
2024-03-16 10:47:39 +0800

    feat: 8-depth buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod texture;
 use cgmath::prelude::*;
-use wgpu::util::{power_preference_from_env, DeviceExt};
+use wgpu::{
+    util::{power_preference_from_env, DeviceExt},
+    Texture,
+};
 use winit::{
     dpi::PhysicalSize,
     event::{self, *},
@@ -113,6 +116,7 @@ struct State {
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
     diffuse_texture: texture::Texture,
+    depth_texture: texture::Texture,
     camera: Camera,
     camera_controller: CameraController,
     camera_uniform: CameraUniform,
@@ -475,6 +479,9 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let depth_texture =
+            texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
@@ -491,11 +498,9 @@ impl State {
                 buffers: &[Vertex::desc(), InstanceRaw::desc()], // 2. type of vertices
             },
             fragment: Some(wgpu::FragmentState {
-                // 3.
                 module: &shader,
                 entry_point: "fs_main",
                 targets: &[Some(wgpu::ColorTargetState {
-                    // 4.
                     format: config.format,
                     blend: Some(wgpu::BlendState::REPLACE), // new data replace old pixel
                     write_mask: wgpu::ColorWrites::ALL,     // rgba
@@ -513,7 +518,13 @@ impl State {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None, // 1. depth buffer
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less, // draw front to back
+                stencil: wgpu::StencilState::default(),     // store depth buffer
+                bias: wgpu::DepthBiasState::default(),
+            }), // 1. depth buffer
             multisample: wgpu::MultisampleState {
                 count: 1,                         // 2. sample in pipeline
                 mask: !0,                         // 3. which sample is active
@@ -549,6 +560,7 @@ impl State {
             num_indices,
             diffuse_bind_group,
             diffuse_texture,
+            depth_texture,
             camera,
             camera_controller,
             camera_uniform,
@@ -568,7 +580,9 @@ impl State {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
-            self.surface.configure(&self.device, &self.config)
+            self.surface.configure(&self.device, &self.config);
+            self.depth_texture =
+                texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -614,7 +628,14 @@ impl State {
                         store: wgpu::StoreOp::Store,
                     },
                 })], // use texture view and put color on it
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store,
+                    }),
+                    stencil_ops: None,
+                }),
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
